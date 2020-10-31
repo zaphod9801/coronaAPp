cb0b3fc5a092629e132c505e467bee45b3cb5408
Johan <jsruiza@eafit.edu.co>
2020-10-31 16:49:12 -0500

    Agregados consejos, sintomas se cambio a una lista de opciones y el sintoma dolor de pecho se agrego

--- tomaDatos/forms.py
+++ tomaDatos/forms.py
@@ -1,11 +1,16 @@
 from django import forms
+opciones = (
+    ('Si','Si'),
+    ('No','No'),
+)
 
 class formSintomas(forms.Form):
-    temperatura = forms.FloatField()
-    dolorGarganta = forms.IntegerField(max_value=5,min_value=0)
-    tos = forms.IntegerField(max_value=5,min_value=0)
-    senGusto = forms.IntegerField(max_value=5,min_value=0)
-    senOlfato = forms.IntegerField(max_value=5,min_value=0)
-    respiracion = forms.IntegerField(max_value=5,min_value=0)
+    temperatura = forms.FloatField(label='Temperatura')
+    dolorGarganta = forms.IntegerField(label='Dolor de garganta',max_value=5,min_value=0)
+    tos = forms.IntegerField(label='Tos',max_value=5,min_value=0)
+    senGusto = forms.ChoiceField(label='Sentido del gusto', widget=forms.Select, choices=opciones)
+    senOlfato = forms.ChoiceField(label='Sentido del olfato', widget=forms.Select, choices=opciones)
+    respiracion = forms.IntegerField(label='Respiracion',max_value=5,min_value=0)
+    dolorPecho = forms.IntegerField(label='Dolor Pecho',max_value=5,min_value=0)
     
     
--- tomaDatos/views.py
+++ tomaDatos/views.py
@@ -6,13 +6,17 @@ from tomaDatos.models import Sintomas
 # Create your views here.
 
 def tomaDatos(request):
-    consejos = [  "Informa a tu médico de familia tu tempuratura" , 
-                "Estás en estado febril, informa a tu médico de familia" , 
+    consejos = [  "Informa a tu médico de familia tu temperatura" , 
+                "Estás en estado febril, toma algún antipilectico, toma una ducha e informa a tu médico de familia" , 
                 "Evita tomar cosas frías" , 
                 "Toma agua al clima y recuerda taparte al toser" , 
                 "Trata de no realizar actividades que te agiten" , 
-                "Llama a emergencia" , 
-                "Quédate en casa, te cuidas y cuidas a los demás" 
+                "Llama a emergencias, tu respiracion esta complicandose" , 
+                "Quédate en casa, te cuidas y cuidas a los demás" ,
+                "Haste una revisión de tu garganta y ante cualquier cosa inusual informa a tu médico de familia",
+                "Toma antigripales y mucho liquido",
+                "Llamar a la eps  y pide indicaciones",
+                "Llama a emergencias, algo malo puede estar pasando"
                 ]
     consejos2 = []
     if request.method == "POST":
@@ -28,13 +32,14 @@ def tomaDatos(request):
                 senGusto = info['senGusto'],
                 senOlfato = info['senOlfato'],
                 respiracion = info['respiracion'],
+                dolorPecho = info['dolorPecho'],
             )
             
-            if (info['temperatura'] <= 34.5) and (info['temperatura'] < 38):
+            if (info['temperatura'] >= 34.5) and (info['temperatura'] < 38):
                 consejos2.append(consejos[0])
             if info['temperatura'] >= 38:
                 consejos2.append(consejos[1])
-            if info['dolorGarganta'] >= 3:
+            if info['dolorGarganta'] == 3:
                 consejos2.append(consejos[2])
             if info['tos'] >= 3:
                 consejos2.append(consejos[3])
@@ -42,7 +47,15 @@ def tomaDatos(request):
                 consejos2.append(consejos[4])
             if info['respiracion']>=4:
                 consejos2.append(consejos[5])
-            
+            if info['dolorGarganta'] >= 4:
+                consejos2.append(consejos[7])
+            if (info['senGusto'] == 'No') or (info['senOlfato'] == 'No'):
+                consejos2.append(consejos[8])
+            if (info['dolorPecho'] < 4) and (info['temperatura'] >= 2):
+                consejos2.append(consejos[9])
+            if info['dolorPecho'] >= 4:
+                consejos2.append(consejos[10])
+                
             contexto = {
                 "consejos":consejos2
             }
